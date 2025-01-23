126e4721a43a50d4cff2315dcea894d7e8f9273b
Samwoker <149298507+Samwoker@users.noreply.github.com>
2025-01-23 07:30:03 -0900

    dynamic routing

--- src/app.js
+++ src/app.js
@@ -1,23 +1,9 @@
 const express = require("express");
 const app = express();
 
-app.use("/user",(req,res)=>{
-  res.send("i dominate everything");
-})
-app.get("/user",(req,res)=>{
-  res.send({firstName: "John", lastName: "Doe"});
-})
-app.post("/user",(req,res)=>{
-  res.send("user created successfully");
-})
-app.delete("/user",(req,res)=>{
-  res.send("user deleted successfully");
-})
-app.patch("/user",(req,res)=>{
-  res.send("user updated successfully");
-})
-app.use("/test", (req, res) => {
-  res.send("testing the test route");
+app.get("/user/:userId/:name/:password", (req, res) => {
+  console.log(req.params);
+  res.send({ firstName: "John", lastName: "Doe" });
 });
 
 app.listen(3000, () => {
