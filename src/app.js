e9605e3baa2bd84d1d12e8728e12ec041ac9c089
Samwoker <149298507+Samwoker@users.noreply.github.com>
2025-01-23 08:44:57 -0900

    multiple routes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,28 @@
 const express = require("express");
 const app = express();
 
-app.get("/user/:userId/:name/:password", (req, res) => {
-  console.log(req.params);
-  res.send({ firstName: "John", lastName: "Doe" });
-});
+app.get(
+  "/user",
+  (req, res, next) => {
+    console.log("handler 1");
+    // res.send("Handle Route 1");
+    next();
+  },
+  (req, res, next) => {
+    console.log("handler 2");
+    next();
+    // res.send("Handle Route 2");
+  },
+  (req, res, next) => {
+    console.log("handler 3");
+    res.send("Handle Route 3");
+    // next();
+  },
+  (req, res) => {
+    console.log("Final handler");
+    res.send("Final handler");
+  }
+);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
