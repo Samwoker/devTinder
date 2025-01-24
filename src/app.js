c58a20b28012da446a231c5ae7ed892667c08fa8
Samwoker <149298507+Samwoker@users.noreply.github.com>
2025-01-24 03:57:52 -0900

    error handling

--- src/app.js
+++ src/app.js
@@ -2,21 +2,21 @@ const express = require("express");
 const app = express();
 const { adminAuth, userAuth } = require("./middlewares/auth");
 
-app.use("/admin", adminAuth);
-app.use("/user", userAuth, (req, res) => {
-  res.send("getting user data");
+app.get("/getUserData", (req, res) => {
+  try{
+    throw new Error("Not implemented");
+    res.send("getting user data");
+  }catch(err){
+   res.status(500).send("some error contact support team");
+  }
+ 
+});
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("something went wrong");
+  }
 });
 
-app.post("/user/login",(req,res)=>{
-  res.send("logging in");
-})
-app.get("/admin/getAllData", (req, res) => {
-  res.send("getting all data");
-});
-
-app.get("/admin/deleteUser", (req, res) => {
-  res.send("deleted user");
-});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
