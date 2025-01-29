d588c0c5667bd0e7e6ad3a38fb2efc05cb5eef37
Samwoker <149298507+Samwoker@users.noreply.github.com>
2025-01-29 09:09:01 -0900

    finding one user and all users using user and feed API

--- src/app.js
+++ src/app.js
@@ -5,6 +5,31 @@ const connectDB = require("./config/database");
 
 app.use(express.json());
 
+
+app.get("/user",async (req,res)=>{
+  const userEmail=req.body.emailId;
+  try{
+  const user=await User.findOne({emailId:userEmail});
+  if(!user){
+    res.status(404).send("no user found");
+  }else{
+    res.send(user);
+  }
+
+  }catch(err){
+     res.status(400).send("something went wrong")
+  }
+})
+app.get("/feed",async (req,res)=>{
+  try{
+  const users=await User.find({});
+  res.send(users);
+
+  }catch(err){
+     res.status(400).send("something went wrong")
+  }
+})
+
 app.post("/signup", async (req, res) => {
   const user = User(req.body);
   try {
